337e88043d36c427da1f18846fa6287350c09316
Sameera-Madusanka <dev.sameera.cs@gmail.com>
2024-01-13 17:12:37 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,11 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-const today = dayjs();
-const deliviryDate = today.add(7,'days');
-console.log(deliviryDate);
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
   let cartSummaryHTML = "";
@@ -115,6 +111,8 @@ export function renderOrderSummary(){
       //get the element
       const containerElement = document.querySelector(`.js-cart-item-container-${productId}`);
       containerElement.remove();
+
+      renderPaymentSummary();
     });
   });
 
@@ -126,7 +124,8 @@ export function renderOrderSummary(){
       //shorthand property
       //const {productId, deleveryOptionId} = elemnet.dataset;
       updateDeliveryOption(productId,deliveryOptionId);
-      renderOrderSummary();   
+      renderOrderSummary();
+      renderPaymentSummary();   
     })
   })
 }
